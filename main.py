2e453a63950678001e1438280b99d6d2f094e235
hammadyaqoob1985 <hammad.yaqoob1985@yahoo.co.uk>
2026-01-31 12:21:47 +0000

    boilerplate code

---
+++ main.py
@@ -0,0 +1,13 @@
+import asyncio
+from dotenv import load_dotenv
+import os
+load_dotenv
+
+
+
+async def main():
+    print("Hello from langchain-course-mcp-servers!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
